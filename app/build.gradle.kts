77d9e0fc8c4abe0ed3624be1d2d0c2f19a07b64d
Wahyu Maulana Aditya <first.wahyumaul21@gmail.com>
2023-12-20 01:50:39 +0700

    Update Code and Layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,7 +73,7 @@ dependencies {
     implementation("com.github.bumptech.glide:glide:4.16.0")
     annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
 
-
+    // CircleImageView
     implementation("de.hdodenhof:circleimageview:3.1.0")
 
     // Firebase
