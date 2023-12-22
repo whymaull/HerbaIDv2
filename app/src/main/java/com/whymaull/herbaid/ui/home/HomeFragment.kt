ca45d9b8ba83e064d6d071cbb85d167ef2f0d666
Wahyu Maulana Aditya <first.wahyumaul21@gmail.com>
2023-12-22 15:51:25 +0700

    Update

--- app/src/main/java/com/whymaull/herbaid/ui/home/HomeFragment.kt
+++ app/src/main/java/com/whymaull/herbaid/ui/home/HomeFragment.kt
@@ -1,7 +1,6 @@
 package com.whymaull.herbaid.ui.home
 
 import android.Manifest
-import android.app.Activity
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
@@ -18,38 +17,26 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.whymaull.herbaid.databinding.FragmentHomeBinding
+import com.whymaull.herbaid.utils.Classifier
 import com.whymaull.herbaid.utils.getImageUri
-import org.tensorflow.lite.Interpreter
-import java.io.FileInputStream
-import java.io.InputStream
-import java.nio.ByteBuffer
-import java.nio.ByteOrder
-import java.nio.MappedByteBuffer
-import java.nio.channels.FileChannel
 
-@Suppress("INTEGER_OVERFLOW", "CAST_NEVER_SUCCEEDS")
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     private var currentImageUri: Uri? = null
     private var outputLabels: List<String>? = null
 
-    private lateinit var interpreter: Interpreter
+    private lateinit var classifier: Classifier
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        interpreter = Interpreter(loadModelFile())
-        outputLabels = readLabels()
+        classifier = Classifier(requireContext().assets, "model.tflite", "label.txt")
         return binding.root
     }
 
@@ -73,7 +60,7 @@ class HomeFragment : Fragment() {
         if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.CAMERA)
             == PackageManager.PERMISSION_GRANTED
         ) {
-            currentImageUri = getImageUri(requireContext(),224, 224, 3)
+            currentImageUri = getImageUri(requireContext(), 224, 224, 3)
             launcherIntentCamera.launch(currentImageUri)
         } else {
             requestCameraPermission.launch(Manifest.permission.CAMERA)
@@ -106,42 +93,14 @@ class HomeFragment : Fragment() {
         Toast.makeText(requireContext(), "Result: $result", Toast.LENGTH_SHORT).show()
     }
 
-    private fun runInference(inputBuffer: ByteBuffer): String {
-        val outputBuffer = ByteBuffer.allocateDirect(OUTPUT_SIZE * 4)
-        interpreter.run(inputBuffer, outputBuffer)
-
-        outputBuffer.rewind()
-        val probabilities = FloatArray(OUTPUT_SIZE)
-        outputBuffer.asFloatBuffer().get(probabilities)
-
-        val maxIndex = probabilities.indices.maxByOrNull { probabilities[it] } ?: -1
-        val resultLabel = outputLabels?.get(maxIndex)
-
-
-
-        return "Class: $resultLabel, Probability: ${probabilities[maxIndex]}"
-
+    private fun runInference(inputBitmap: Bitmap): String {
+        val result = classifier.recognizeImage(inputBitmap)
+        return result.joinToString { it.toString() }
     }
 
-    private fun preprocessImage(uri: Uri?): ByteBuffer {
-        val inputStream: InputStream? = uri?.let { requireContext().contentResolver.openInputStream(it) }
-        val originalBitmap = BitmapFactory.decodeStream(inputStream)
-
-        val resizedBitmap = Bitmap.createScaledBitmap(originalBitmap, INPUT_WIDTH, INPUT_HEIGHT, true)
-
-        val byteBuffer = ByteBuffer.allocateDirect(INPUT_SIZE * 4)
-        byteBuffer.order(ByteOrder.nativeOrder())
-
-        val pixels = IntArray(INPUT_WIDTH * INPUT_HEIGHT)
-        resizedBitmap.getPixels(pixels, 0, resizedBitmap.width, 0, 0, resizedBitmap.width, resizedBitmap.height)
-
-        for (pixelValue in pixels) {
-            val normalizedValue = (pixelValue and 0xFF) / 255.0f
-            byteBuffer.putFloat(normalizedValue)
-        }
-        byteBuffer.rewind()
-
-        return byteBuffer
+    private fun preprocessImage(uri: Uri?): Bitmap {
+        val inputStream = uri?.let { requireContext().contentResolver.openInputStream(it) }
+        return BitmapFactory.decodeStream(inputStream)
     }
 
     private fun startGallery() {
@@ -154,7 +113,7 @@ class HomeFragment : Fragment() {
     private val launcherGallery = registerForActivityResult(
         ActivityResultContracts.StartActivityForResult()
     ) { result: ActivityResult ->
-        if (result.resultCode == Activity.RESULT_OK) {
+        if (result.resultCode == android.app.Activity.RESULT_OK) {
             result.data?.data?.let { uri ->
                 currentImageUri = uri
                 showImage()
@@ -163,45 +122,12 @@ class HomeFragment : Fragment() {
             Log.d("Photo Picker", "No media selected")
         }
     }
+
     private fun showImage() {
         currentImageUri?.let {
             Log.d("Image URI", "showImage: $it")
             binding.imgUploaded.setImageURI(it)
             processImage()
         }
-    }
-
-    private fun loadModelFile(): MappedByteBuffer {
-        val assetManager = requireContext().assets
-        val modelFilename = "model.tflite"
-        val fileDescriptor = assetManager.openFd(modelFilename)
-
-        FileInputStream(fileDescriptor.fileDescriptor).use { inputStream ->
-            val fileChannel: FileChannel = inputStream.channel
-            val startOffset = fileDescriptor.startOffset
-            val declaredLength = fileDescriptor.declaredLength
-
-            return fileChannel.map(FileChannel.MapMode.READ_ONLY, startOffset, declaredLength)
-        }
-    }
-
-    private fun readLabels(): List<String> {
-        val labelFilename = "label.txt"
-        val inputStream: InputStream = requireContext().assets.open(labelFilename)
-        val labels = mutableListOf<String>()
-
-        inputStream.bufferedReader().useLines { lines ->
-            lines.forEach { labels.add(it) }
-        }
-
-        return labels
-    }
-
-    companion object {
-        private const val INPUT_WIDTH = 224
-        private const val INPUT_HEIGHT = 224
-        private const val PIXEL_SIZE = 3
-        private const val INPUT_SIZE = INPUT_WIDTH * INPUT_HEIGHT * PIXEL_SIZE
-        private const val OUTPUT_SIZE = 10
     }
 }
